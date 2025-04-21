1b70b2748a6ee95469b5e4d8b0bea6773f1dc599
Luke Truesdale <luke-truesdale@uiowa.edu>
2025-04-21 09:30:47 -0500

    Update URL

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = "seasdad(*2sffcra01^23sdet"
 CORS(app)
 
 # Get this URL from the Azure Overview page of your API web app
-api_url = "ltruesdale-api-2ndattempt-hseadwhaaecdgdgx.eastus-01.azurewebsites.net"  # base url for API endpoints
+api_url = "https://ltruesdale-api-2ndattempt-hseadwhaaecdgdgx.eastus-01.azurewebsites.net/"  # base url for API endpoints
 
 
 # main index page route
